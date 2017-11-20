0dbe5b98d6ca67001469193a294fdf4f785c3402
stopvoldemort <david.squires1@gmail.com>
2017-11-20 10:08:07 -0500

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  state = {
+    username: "",
+    hometown: ""
+  }
+
+  handleNameChange = (ev) => {this.setState({username: ev.target.value})}
+
+  handleTownChange = (ev) => {this.setState({hometown: ev.target.value})}
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: this.state
+    })
+    this.setState({username: "", hometown: ""}, () => {
+      console.log(this.props.store.getState())
+    })
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit}>
+          <label>Username: </label>
+          <input onChange={this.handleNameChange} type="text" id="username" value={this.state.username} />
+          <label>Hometown: </label>
+          <input onChange={this.handleTownChange} type="text" id="hometown" value={this.state.hometown} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,9 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {users: []}, action){
+  switch (action.type) {
+    case "ADD_USER":
+      const newUsers = [...state.users, action.user]
+      return Object.assign({}, state, {users: newUsers})
+    default:
+      return state
+  }
 }
